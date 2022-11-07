497a4ddaf8bae4a59be3551e6cbbbf914b64cb21
Nicolas <nicolas.franco@etu.uca.fr>
2022-11-07 11:21:50 +0100

    new auth method system

--- lib/Classes/accountManager.dart
+++ lib/Classes/accountManager.dart
@@ -14,13 +14,24 @@ class AccountManager{
 
   static void chooseAuthentificationMethod(Account a, int i){
     if(i == 1) {
-      a.authMethod = "conventional";
+      switch_auth_method(a.authMethod, "conventionalsss");
     }
     if(i == 2) {
-      a.authMethod = "yubikey_only";
+      switch_auth_method(a.authMethod, "yubikey_only");
     }
     if(i == 3) {
-      a.authMethod = "twoFA_with_yubikey";
+      switch_auth_method(a.authMethod, "twoFA_with_yubikey");
     }
+  }
+
+  static void switch_auth_method(Map<String,bool> m, String method){
+    m.forEach((key, value) {
+      if(key == method){
+        value = true;
+      }
+      else {
+        value = false;
+      }
+    });
   }
 }

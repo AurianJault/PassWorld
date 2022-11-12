99e7323a2e4dcc76335cae7433cf4c51016fcb1a
Tim <tim.levadoux@etu.uca.fr>
2022-11-12 17:17:13 +0100

    Classe de chiffrement de fichier avec gestion des exceptions

--- lib/Classes/fileEncrypt.dart
+++ lib/Classes/fileEncrypt.dart
@@ -1,56 +1,58 @@
 import 'dart:io';
 import 'package:aes_crypt/aes_crypt.dart';
+import 'package:flutter/widgets.dart';
+import 'dart:typed_data';
 import 'storage.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-class EncryptFile{
+class EncryptFile implements Exception{
   // ignore: non_constant_identifier_names
   static void encrypt_file(String path) async{
     AesCrypt crypt = AesCrypt();
-    crypt.setOverwriteMode(AesCryptOwMode.on);
+    //crypt.setOverwriteMode(AesCryptOwMode.on);
     crypt.setPassword('my cool password');
     String encFilepath;
     try {
       encFilepath = crypt.encryptTextToFileSync((await Storage.getKey("Pierre")).base64, path);
       print('The encryption has been completed successfully.');
       print('Encrypted file: $encFilepath');
-    } catch (e) {
-      /*if (e.type == AesCryptExceptionType.destFileExists) {
+    } on AesCryptException catch (e) {
+      if (e.type == AesCryptExceptionType.destFileExists) {
         print('The encryption has been completed unsuccessfully.');
         print(e.message);
       }
       else{
         // message d'erreur
-      }*/
+      }
     }
   }
 
-  static String decrypt_file(String path) {
+  static String? decrypt_file(String path) {
     AesCrypt crypt = AesCrypt();
     crypt.setOverwriteMode(AesCryptOwMode.on);
     crypt.setPassword('my cool password');
     String key;
-    try {
+    try{
       key = crypt.decryptTextFromFileSync(path);
       print('The decryption has been completed successfully.');
       print('Decrypted file 1: $path');
       print('File content: ' + File(path).path);
       return key;
-    } catch (e) {
-      if (e == AesCryptExceptionType.destFileExists) {
-        print('The decryption has been completed unsuccessfully.');
-        print('e.message');
-        throw(e);
+    } on AesCryptException catch (e){
+      if (e.type == AesCryptExceptionType.destFileExists) {
+        print('The encryption has been completed unsuccessfully.');
+        print(e.message);
       }
       else{
-        return 'ERROR';
+        // message d'erreur
       }
-    }
+    }  
   }
 }
 
-void main(List<String> args) {
+/*
+void main(List<String> args) async{
   TestWidgetsFlutterBinding.ensureInitialized();
-  Storage.ajouterKeyIV("Pierre");
-  print(EncryptFile.decrypt_file("./lib/test.txt"));
-}
+  var test = EncryptFile.encrypt_file("test.txt");
+  //print(test);
+}*/

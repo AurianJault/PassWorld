24450ed49e2503f93b4858df6e8c2346648835e2
aurian.jault <aurian.jault@etu.uca.fr>
2022-11-16 17:44:29 +0100

    Ajout première fonction Doctor

--- lib/Classes/doctor.dart
+++ lib/Classes/doctor.dart
@@ -1,8 +1,28 @@
-class Doctor{
+import 'package:test/Classes/account.dart';
+import 'package:test/Classes/password.dart';
+
+class Doctor {
   // list des fonctions a faire
-    // Check nbr de fois used pour un mdp
+  // Check nbr de fois used pour un mdp
+  static List reused(Account user) {
+    List<Password> list = user.vlt.passwordList;
+    var res = [];
+    // Pour chaque element de la list :
+    // Check si ce mdp est plus d'une fois dans la même liste
+    list.forEach((element) {
+      int count = 0;
+      var pwd = element.getPassword;
+      for (var i in list) {
+        if (i.getPassword == pwd) count++;
+      }
+      if (count > 1) {
+        if (!res.contains(element))res.add(element);
+      }
+    });
+    return res;
+  }
+  // Check la force de chaque mdp
+  
 
-    // Check la force de chaque mdp
-
-    // Si le mdp est updated
+  // Si le mdp est updated
 }

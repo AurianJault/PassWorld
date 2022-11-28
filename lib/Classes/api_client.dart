7fed968ad5550edfd9175021667ca45735709699
RemRem <remrem@github.com>
2022-11-28 18:02:17 +0100

    tw

--- lib/Classes/api_client.dart
+++ lib/Classes/api_client.dart
@@ -1,23 +1,25 @@
 import 'dart:html';
-
 import 'package:http/http.dart' as http;
 
 class ClientAPI {
   // Base URL of API
-  String base = '';
+  String base =
+      'https://codefirst.iut.uca.fr/containers/passworld-api-remiarnal';
   // GET
 
   // Athenticate user when signing in
   Future<bool> authenticator() async {
     Uri url = "$base/auth" as Uri;
-    var response = await http.get(url);
-
-    /*
+    var mail = ""; // TODO
+    var password = ""; // TODO
+    String body = """
+    {
+      "mail" : "$mail",
+      "password" : "$password" 
+    }""";
+    var response = await http.post(url, body: body);
     print('Response status: ${response.statusCode}');
     print('Response body: ${response.body}');
-    print(await http.read('https://example.com/foobar.txt'));
-    */
-
     return true;
   }
 }

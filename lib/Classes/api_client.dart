68e6699818493928a6d1583a3a9d0f57478546bd
RemRem <remrem@github.com>
2022-12-09 11:27:24 +0100

    change to master

--- lib/Classes/api_client.dart
+++ lib/Classes/api_client.dart
@@ -1,10 +1,19 @@
+import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
 
 class ClientAPI {
   // Base URL of API
-  static String base =
-      'https://codefirst.iut.uca.fr/containers/passworld-api-remiarnal';
+  // static String base =
+  //     'https://codefirst.iut.uca.fr/containers/passworld-api-remiarnal';
+
+  static String base = 'http://172.27.8.3:8989';
+
+  static Future<Response> root() async {
+    Uri url = Uri.parse(base);
+    var response = await http.get(url);
+    return response;
+  }
   // POST
 
   // Athenticate user when signing in
@@ -17,6 +26,25 @@ class ClientAPI {
     }
     """;
     var response = await http.post(url, body: body);
+    return response;
+  }
+
+  static Future<StreamedResponse> uploadFile(
+      String mail, String password, File password_file) async {
+    print(password_file.length());
+    Stream<List<int>> streamFile = password_file.openRead();
+    Uri url = Uri.parse("$base/user/password-file");
+    String bodyy = """
+    {
+      "email" : "$mail",
+      "password" : "$password"
+    }
+    """;
+    //var response = http.put(url, body: streamFile);
+    var request = http.MultipartRequest("PUT", url);
+    var passwordPart = http.MultipartFile("file", streamFile, 9951);
+    request.files.add(passwordPart);
+    var response = await request.send();
     return response;
   }
 }
--- test/api_test.dart
+++ test/api_test.dart
@@ -1,8 +1,29 @@
+import 'dart:io';
+
 import 'package:test/Classes/api_client.dart';
 
-void main() async {
-  // autentication test
+void test_api_auth() async {
   var req1 = await ClientAPI.authenticator("remrem@gmail.com", "password123");
   print(req1.statusCode);
   print(req1.body);
+}
+
+Future<int> test_api_file_upload() async {
+  print("TEST: API FILE UPLOAD BEGIN");
+  File file = File("/home/hel/Projets/PassWorld/assets/bereal.png");
+  print("Phone: uploading file");
+  var req1 =
+      await ClientAPI.uploadFile("remrem@gmail.com", "password123", file);
+  print("Phone: file uploaded");
+  //print(req1.statusCode);
+  //print(req1.stream);
+  print("TEST: API FILE UPLOAD END");
+  return 6;
+}
+
+void main() async {
+  // var res = await ClientAPI.root();
+  // print(res.body);
+  var haha = await test_api_file_upload();
+  // autentication test
 }

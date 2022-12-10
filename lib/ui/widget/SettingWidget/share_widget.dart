246a62b9571abd10e44113426628ea1e5f560ba4
Tim pro <tim.levadoux@etu.uca.fr>
2022-12-10 15:35:27 +0100

    ajout du file picker

--- lib/ui/widget/SettingWidget/share_widget.dart
+++ lib/ui/widget/SettingWidget/share_widget.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:share_plus/share_plus.dart';
+import 'package:file_picker/file_picker.dart';
 
 import '../../../Classes/Exception/storageException.dart';
 import '../../../Classes/account.dart';
@@ -25,11 +27,20 @@ showShare(BuildContext context) async {
             Navigator.pop(context);
             try {
               if (Platform.isLinux || Platform.isWindows) {
-                File("path").writeAsStringSync(
-                    (await Storage.getKey(context.read<Account>().id)).base64 +
-                        (await Storage.getIV(context.read<Account>().id))
-                            .base64,
-                    mode: FileMode.writeOnly);
+                Directory? init = await getDownloadsDirectory();
+                String? output = await FilePicker.platform.saveFile(
+                    dialogTitle: 'Please select an output file',
+                    fileName: 'passworld.pdf',
+                    initialDirectory: (init)!.path);
+                if (output != null) {
+                  File(output).writeAsStringSync(
+                      (await Storage.getKey(context.read<Account>().id))
+                              .base64 +
+                          (await Storage.getIV(context.read<Account>().id))
+                              .base64,
+                      mode: FileMode.writeOnly);
+                }
+                return;
               } else {
                 await Share.share(
                     (await Storage.getKey(context.read<Account>().id)).base64 +

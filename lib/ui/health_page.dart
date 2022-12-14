8ff5522428242582e69abff3c1678162a4e6ccef
aurian.jault <aurian.jault@etu.uca.fr>
2022-12-14 11:15:01 +0100

    DoctorPassword widget AND Doctor view ended

--- lib/ui/health_page.dart
+++ lib/ui/health_page.dart
@@ -6,6 +6,7 @@ import 'package:test/Classes/strength_password.dart';
 import 'package:test/Classes/vault.dart';
 import 'package:test/ui/widget/page_title_widget.dart';
 import 'package:test/ui/widget/password_widget.dart';
+import 'package:test/ui/widget/red_password.dart';
 
 import '../Classes/account.dart';
 
@@ -20,7 +21,8 @@ class _HealthPageState extends State<HealthPage> {
   @override
   Widget build(BuildContext context) {
     // Passwords's Strenght
-    List list = Doctor.strenght(context.read<Account>());
+    List<List<Password>> list = Doctor.strenght(context.read<Account>());
+    Password selected;
     // Media Query
     var size = MediaQuery.of(context).size;
     var w = size.width;
@@ -54,6 +56,152 @@ class _HealthPageState extends State<HealthPage> {
           const SizedBox(
             width: 20,
           ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(25),
+                child: Text(
+                  "Critical password(s) strenght",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: w * 0.035),
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            width: 15,
+          ),
+          Container(
+            color: Colors.red,
+            height: 50,
+            width: w * 0.80,
+            child: DropdownButton<Password>(
+                icon: const Icon(Icons.arrow_drop_down),
+                items: list[0]
+                    .map<DropdownMenuItem<Password>>(
+                        (val) => DropdownMenuItem<Password>(
+                            value: val,
+                            child: Padding(
+                              padding: const EdgeInsets.all(5),
+                              child: DoctorPassword(
+                                password: val,
+                                color: Colors.red,
+                              ),
+                            )))
+                    .toList(),
+                onChanged: ((value) {})),
+          ),
+          const SizedBox(
+            width: 100,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(25),
+                child: Text(
+                  "Medium password(s) strenght",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: w * 0.035),
+                ),
+              )
+            ],
+          ),
+          Container(
+            color: Colors.orange,
+            height: 50,
+            width: w * 0.80,
+            child: DropdownButton<Password>(
+                icon: const Icon(Icons.arrow_drop_down),
+                items: list[1]
+                    .map<DropdownMenuItem<Password>>((val) =>
+                        DropdownMenuItem<Password>(
+                            value: val,
+                            child: Padding(
+                                padding: const EdgeInsets.all(5),
+                                child: DoctorPassword(
+                                    password: val, color: Colors.orange))))
+                    .toList(),
+                onChanged: ((value) {})),
+          ),
+          const SizedBox(
+            width: 100,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(25),
+                child: Text(
+                  "Reused password(s)",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: w * 0.035),
+                ),
+              )
+            ],
+          ),
+
+          Container(
+            color: Colors.blueGrey,
+            height: 50,
+            width: w * 0.80,
+            child: DropdownButton<Password>(
+                icon: const Icon(Icons.arrow_drop_down),
+                items: Doctor.reused(context.read<Account>())
+                    .map<DropdownMenuItem<Password>>((val) =>
+                        DropdownMenuItem<Password>(
+                            value: val,
+                            child: Padding(
+                                padding: const EdgeInsets.all(5),
+                                child: DoctorPassword(
+                                    password: val, color: Colors.orange))))
+                    .toList(),
+                onChanged: ((value) {})),
+          ),
+
+
+
+          const SizedBox(
+            width: 100,
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(25),
+                child: Text(
+                  "Too old password(s)",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: w * 0.035),
+                ),
+              )
+            ],
+          ),
+
+          Container(
+            color: Colors.lightGreen,
+            height: 50,
+            width: w * 0.80,
+            child: DropdownButton<Password>(
+                icon: const Icon(Icons.arrow_drop_down),
+                items: Doctor.timeUsed(context.read<Account>())
+                    .map<DropdownMenuItem<Password>>((val) =>
+                        DropdownMenuItem<Password>(
+                            value: val,
+                            child: Padding(
+                                padding: const EdgeInsets.all(5),
+                                child: DoctorPassword(
+                                    password: val, color: Colors.orange))))
+                    .toList(),
+                onChanged: ((value) {})),
+          ),
+
+
         ],
       )),
     );

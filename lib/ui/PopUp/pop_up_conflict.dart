e64704fca25bf492cd39e753c7f3dcdb2632799a
Corentin "Koroh" RICHARD <corentin.richard@etu.uca.fr>
2023-01-05 15:01:45 +0100

    Adding visual for conflict manager

--- lib/ui/PopUp/pop_up_conflict.dart
+++ lib/ui/PopUp/pop_up_conflict.dart
@@ -3,48 +3,87 @@ import 'package:io/ansi.dart';
 import 'package:password_strength_checker/password_strength_checker.dart';
 import 'package:test/Classes/password.dart';
 
-showConflictDialog(BuildContext context,Password passDown,Password passSaved) {
-  var frstState = '❌';
-  var scndState = '❌';
+int showConflictDialog(
+    BuildContext context, Password passDown, Password passSaved) {
+  var frstState = false;
+  var fSign = '❌';
+  var fColor = Color.fromARGB(120, 190, 20, 20);
+  var scndState = false;
+  var sSign = '❌';
+  var sColor = Color.fromARGB(120, 190, 20, 20);
 
-  //✔️
+  //✔️❌
 
   // set up the button
   var col = Column(mainAxisAlignment: MainAxisAlignment.center, children: [
     Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child:  
-        TextButton(
-          onPressed: () => Navigator.pop(context),
-          child: 
-            Text('$frstState ${passDown.getName}'),
-        ),
-    ),
-    
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: Container(
+            alignment: Alignment.centerLeft,
+            //decoration: const BoxDecoration(color: Colors.white),
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: fColor),
+              onPressed: () => {
+                TextButton.styleFrom(
+                    backgroundColor: Color.fromARGB(143, 81, 231, 35)),
+                if (frstState == false)
+                  {
+                    frstState = !frstState,
+                    scndState = false,
+                    fSign = '',
+                    fColor = Color.fromARGB(120, 190, 20, 20)
+                  }
+                else
+                  {fSign = '', fColor = Color.fromARGB(143, 81, 231, 35)}
+              },
+              child: Text('${passDown.getName} : ${passDown.getPassword}'),
+            ))),
     Padding(
-      
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child:
-      Container(
-        alignment: 
-          Alignment.centerLeft,
-        decoration: const BoxDecoration(
-          color: Colors.white),
-        child: 
-      
-        
-        TextButton(
-          onPressed: () => Navigator.pop(context),
-          child: 
-            Text('$scndState ${passSaved.getName}'),
-        )
-      )
-    )
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: Container(
+            alignment: Alignment.centerRight,
+            //decoration: const BoxDecoration(color: Colors.white),
+            child: TextButton(
+              style: TextButton.styleFrom(backgroundColor: sColor),
+              onPressed: () => {
+                if (scndState == false)
+                  {
+                    scndState = !scndState,
+                    frstState = false,
+                    sSign = '',
+                    sColor = Color.fromARGB(120, 190, 20, 20)
+                  }
+                else
+                  {sSign = '', sColor = Color.fromARGB(143, 81, 231, 35)}
+              },
+              child: Text('${passSaved.getName} : ${passSaved.getPassword} '),
+            ))),
+    Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 5.0),
+        child: Container(
+            alignment: Alignment.center,
+            //decoration:
+            //    const BoxDecoration(color: Color.fromARGB(223, 224, 224, 224)),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 230, 230, 230)),
+              onPressed: () => {
+                if (frstState == true || scndState == true)
+                  {
+                    Navigator.pop(context, {'saved': frstState})
+                  }
+                else
+                  {
+                    //print error message to select
+                  }
+              },
+              child: const Text("Ok"),
+            )))
   ]);
 
   // set up the AlertDialog
-  AlertDialog alert = AlertDialog(  
-    title:const Text("Conflicts : "),
+  AlertDialog alert = AlertDialog(
+    title: const Text("Conflicts : "),
     actions: [
       col,
     ],
@@ -57,5 +96,6 @@ showConflictDialog(BuildContext context,Password passDown,Password passSaved) {
       return alert;
     },
   );
-}
 
+  return 0;
+}
